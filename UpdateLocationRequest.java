b0f69949c7aa32c4cf21277ff1d8f6f84bd48f0c
sulafaleryani <eryanisulaf@gmail.com>
2026-06-03 15:12:40 +0300

    Create UpdateLocationRequest.java

---
+++ UpdateLocationRequest.java
@@ -0,0 +1,14 @@
+package com.rideshare.dto.request;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+@Data
+public class UpdateLocationRequest {
+
+    @NotNull(message = "Latitude is required")
+    private Double latitude;
+
+    @NotNull(message = "Longitude is required")
+    private Double longitude;
+}
